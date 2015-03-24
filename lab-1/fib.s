a175fb43f189b1b0ee32c19263edfa45c2081c37
annj <atlantis09>
2015-03-24 22:41:16 +0800

    Use thumb-2 conditional execution to implement

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -10,34 +10,33 @@
 
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
+	
+
+	@ if R0 = 0, return 0
+        cmp    R0, #0
+	itt    le
+        movle  R0, #0
+	bxle   lr
+	@ if R0 = 1, return 1
+	cmp    R0, #1
+	it     eq
+        bxeq   lr
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
+	push   {r4, r5, lr}
+	@ R4 = R0
+        movs   R4, R0
 	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
+	subs   R0, R4, #1
+	bl    fibonacci
+	@ Value return from fibonacci stores in R0. Add the sum in R5
+	mov    R5, R0
 	@ Recursive call to fibonacci with R4 - 2 as parameter
-
+	sub   R0, R4, #2
+        bl   fibonacci
 	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+        adds R0, R0, R5
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
