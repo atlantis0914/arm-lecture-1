050a6320fc2bae1e33ae6530cdef4f9389afcb82
annj <atlantis09>
2015-03-30 22:48:42 +0800

    Iterative version of fibonacci

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,25 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	
+	
+	
+	mov   R3, #0
+	mov   R4, #1
+	mov   R5, R3
+.loop:
+	subs	R0, #1
+	ittt	le
+	movle	R0, R5
+	pople	{r3, r4, r5, pc}
+	bxle	lr
+	
+	add	R5, R3, R4
+	mov	R3, R4
+	mov	R4, R5
+	b	.loop
+	
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
